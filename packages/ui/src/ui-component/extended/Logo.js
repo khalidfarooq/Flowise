54006f588f95635cf2dfbf413139c85e514ad628
khalid farooq <47777912+khalidfarooq@users.noreply.github.com>
2023-06-04 16:49:56 +0530

    Update Logo.js

--- packages/ui/src/ui-component/extended/Logo.js
+++ packages/ui/src/ui-component/extended/Logo.js
@@ -9,7 +9,7 @@ const Logo = () => {
     const customization = useSelector((state) => state.customization)
 
     return (
-         <div style={{display:'none', alignItems: 'center', display: 'flex', flexDirection: 'row' }}>
+         <div style={{display:'none', alignItems: 'center', flexDirection: 'row' }}>
             <img
                 style={{ objectFit: 'contain', height: 'auto', width: 150 }}
                 src={customization.isDarkMode ? logoDark : logo}
